ae26ec9e3a586436a1a20f5b2c97bd98bd264139
VarshaNachiyar14 <160300056+VarshaNachiyar14@users.noreply.github.com>
2024-06-24 15:37:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById("toggle");
+const navigation = document.getElementById("nav");
+
+toggleButton.addEventListener("click", () => navigation.classList.toggle("active"));
+
+
